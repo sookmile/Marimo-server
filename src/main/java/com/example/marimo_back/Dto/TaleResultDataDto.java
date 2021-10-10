f26344d46fbe9b675407e7c1905290e1130ea155
ej11240 <hwangej1124@gmail.com>
2021-10-11 00:59:38 +0900

    fix: tale 주소 수정

--- src/main/java/com/example/marimo_back/Dto/TaleResultDataDto.java
+++ src/main/java/com/example/marimo_back/Dto/TaleResultDataDto.java
@@ -10,8 +10,6 @@ public class TaleResultDataDto {
 
     private Long userId;
 
-//    private Long id;
-
     private String taleName;
 
     private Integer lastpage;
--- src/main/java/com/example/marimo_back/MarimoBackApplication.java
+++ src/main/java/com/example/marimo_back/MarimoBackApplication.java
@@ -2,9 +2,12 @@ package com.example.marimo_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
-public class MarimoBackApplication {
+public class MarimoBackApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MarimoBackApplication.class, args);
