eb1a8553bd2a192b685baf0805efbdc3f9407ec8
ej11240 <hwangej1124@gmail.com>
2021-10-10 15:09:57 +0900

    feat: 동화 영상링크api수정

--- src/main/java/com/example/marimo_back/controller/TaleController.java
+++ src/main/java/com/example/marimo_back/controller/TaleController.java
@@ -43,14 +43,33 @@ public class TaleController {
     public String sendSpeeachUri (@RequestBody Map<String,String> oWord) {
 
         String[] requestList={"침대", "이불", "시계", "축구공", "액자", "냉장고", "사과", "포도", "수박", "바나나", "달력", "기차", "자전거", "색연필", "도토리", "장갑"};
+        String[] requestLinks= {
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%8E%E1%85%B5%E1%86%B7%E1%84%83%E1%85%A2.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%8B%E1%85%B5%E1%84%87%E1%85%AE%E1%86%AF.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%89%E1%85%B5%E1%84%80%E1%85%A8.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%8E%E1%85%AE%E1%86%A8%E1%84%80%E1%85%AE%E1%84%80%E1%85%A9%E1%86%BC.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%8B%E1%85%A2%E1%86%A8%E1%84%8C%E1%85%A1.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%82%E1%85%A2%E1%86%BC%E1%84%8C%E1%85%A1%E1%86%BC%E1%84%80%E1%85%A9.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%89%E1%85%A1%E1%84%80%E1%85%AA.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%91%E1%85%A9%E1%84%83%E1%85%A9.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%89%E1%85%AE%E1%84%87%E1%85%A1%E1%86%A8.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%87%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%A1.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%83%E1%85%A1%E1%86%AF%E1%84%85%E1%85%A7%E1%86%A8.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%80%E1%85%B5%E1%84%8E%E1%85%A1.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%8C%E1%85%A1%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%80%E1%85%A5.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%89%E1%85%A2%E1%86%A8%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%91%E1%85%B5%E1%86%AF.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%83%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A5_%E1%84%83%E1%85%A9%E1%84%90%E1%85%A9%E1%84%85%E1%85%B5.mp4",
+                "https://storage.googleapis.com/marimo_bucket/video/%E1%84%8C%E1%85%A1%E1%86%BC%E1%84%80%E1%85%A1%E1%86%B8.mp4",
+        };
+
         for(int i=0; i<requestList.length; i++){
             if(oWord.get("word").equals(requestList[i])){
                 System.out.println(requestList[i]);
-//                return "http://"+requestList[i];
-                return "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+                return requestLinks[i];
+
             }
         }
         return "";
     }
-//    침대, 이불, 시계, 축구공, 액자, 냉장고, 사과포도, 수박, 바나나, 달력, 기차, 자전거, 색연필, 도토리, 장갑
+
 }
